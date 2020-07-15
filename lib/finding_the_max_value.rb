d66badf9f1181b32e90587871286f07f7c66bfb0
JoAnna Park <joannabpark1@gmail.com>
2020-07-15 18:23:17 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,6 +2,10 @@ def find_max_value(array)
   count = 0
   max_value = nil
   while count < array.length do
-    if array[count] == 
-    
+    if array[count] == 5
+      max_value = count
+    end
+    count += 1
+end
+max_value
 end
